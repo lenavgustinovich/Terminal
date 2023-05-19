eee5f321f3f9eeba3b9daaa5fb8c595e79ba399c
Elena Avgustinovich1992 <lena.leno430@gmail.com>
2023-05-19 18:24:49 +0300

    script

---
+++ shh.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+&& cd homework
+&& mkdir test_1 test_2 test_3
+&& cd test_2
+&& touch q.txt w.txt e.txt a.json s.json
+&& mkdir folder_1 folder_2 folder_3
+&& ls
+&& mv 1.txt 1.json folder_1/
+
